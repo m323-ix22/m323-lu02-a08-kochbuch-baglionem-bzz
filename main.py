6e6d0b7a0e6971cc9b9f73bc75083ef57a39b450
max_b <baglionem@bzz.ch>
2024-08-26 12:00:00 +0200

    update to 100%

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+"""
+Dieses Modul verwaltet Rezepte und passt Mengenangaben basierend auf der Anzahl der Personen an.
+"""
+
 import json
+
+
 def load_recipe(json_string):
     """
     Diese Funktion nimmt einen JSON-String als Argument und wandelt ihn in ein Python-Dictionary um.
@@ -8,64 +14,49 @@ def load_recipe(json_string):
     return json.loads(json_string)
 
 
-def adjust_recipe(recipe, new_servings):
+def adjust_recipe(recipe_data, new_servings):
     """
     Diese Funktion passt die Mengenangaben eines Rezepts an eine neue Anzahl von Personen an.
-    :param recipe: Ein Dictionary, das das Rezept enthält, einschließlich der Zutaten und Portionen.
+    :param recipe_data: Ein Dictionary, das das Rezept enthält, einschließlich der Zutaten und Portionen.
     :param new_servings: Die neue Anzahl von Personen, für die das Rezept angepasst werden soll.
     :return: Ein neues Dictionary mit den angepassten Mengenangaben.
     """
-    original_servings = recipe['servings']
+    original_servings = recipe_data['servings']
     adjustment_factor = new_servings / original_servings
 
     adjusted_ingredients = {
         ingredient: quantity * adjustment_factor
-        for ingredient, quantity in recipe['ingredients'].items()
+        for ingredient, quantity in recipe_data['ingredients'].items()
     }
 
-    adjusted_recipe = {
-        'title': recipe['title'],
+    adjusted_recipe_data = {
+        'title': recipe_data['title'],
         'ingredients': adjusted_ingredients,
         'servings': new_servings
     }
 
-    return adjusted_recipe
+    return adjusted_recipe_data
 
-
-def adjust_recipe(recipe, new_servings):
-    """
-    Diese Funktion passt die Mengenangaben eines Rezepts an eine neue Anzahl von Personen an.
-    :param recipe: Ein Dictionary, das das Rezept enthält, einschließlich der Zutaten und Portionen.
-    :param new_servings: Die neue Anzahl von Personen, für die das Rezept angepasst werden soll.
-    :return: Ein neues Dictionary mit den angepassten Mengenangaben.
-    """
-    original_servings = recipe['servings']
-    adjustment_factor = new_servings / original_servings
-
-    adjusted_ingredients = {
-        ingredient: quantity * adjustment_factor
-        for ingredient, quantity in recipe['ingredients'].items()
-    }
-
-    adjusted_recipe = {
-        'title': recipe['title'],
-        'ingredients': adjusted_ingredients,
-        'servings': new_servings
-    }
-
-    return adjusted_recipe
-
-
-# Dein Code kommt hier hin
 
 if __name__ == '__main__':
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    # Beispiel für die Datenstruktur eines Rezepts
+    recipe_json = (
+        '{"title": "Spaghetti Bolognese", '
+        '\'ingredients\': {\'Spaghetti\': 400, \'Tomato Sauce\': 300, \'Minced Meat\': 500}, '
+        '\'servings\': 4}'
+    )
 
-    recipe = load_recipe(recipe_json)
-    print("Originales Rezept:", recipe)
+    # Rezept laden
+    original_recipe = load_recipe(recipe_json)
+    print('Originales Rezept:')
+    print(json.dumps(original_recipe, indent=4))
 
-    adjusted_recipe = adjust_recipe(recipe, 2)
-    print("Angepasstes Rezept für 2 Personen:", adjusted_recipe)
+    # Rezept für 2 Personen anpassen
+    adjusted_recipe_2 = adjust_recipe(original_recipe, 2)
+    print('\nAngepasstes Rezept für 2 Personen:')
+    print(json.dumps(adjusted_recipe_2, indent=4))
 
-    adjusted_recipe_6 = adjust_recipe(recipe, 6)
-    print("Angepasstes Rezept für 6 Personen:", adjusted_recipe_6)
+    # Rezept für 6 Personen anpassen
+    adjusted_recipe_6 = adjust_recipe(original_recipe, 6)
+    print('\nAngepasstes Rezept für 6 Personen:')
+    print(json.dumps(adjusted_recipe_6, indent=4))
